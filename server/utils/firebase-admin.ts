40219b606bceedb2ce31462b524bb32f427d5809
ChiNg22nd04 <dedonai99@gmail.com>
2025-08-18 14:21:43 +0700

    fix env firebase admin

--- server/utils/firebase-admin.ts
+++ server/utils/firebase-admin.ts
@@ -1,11 +1,11 @@
 // server/utils/firebase-admin.ts
 import admin, { ServiceAccount } from 'firebase-admin';
-import serviceAccount from '../secrets/firebase-service-account.json';
-
-const serviceAccountTyped = serviceAccount as ServiceAccount;
+// import serviceAccount from '../secrets/firebase-service-account.json';
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
+// const serviceAccountTyped = serviceAccount as ServiceAccount;
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountTyped),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
