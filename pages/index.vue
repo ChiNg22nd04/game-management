8186cac763b1c8536f9207e74850ab09337e0224
ChiNg22nd04 <dedonai99@gmail.com>
2025-08-19 21:17:17 +0700

    update auto navigation api/games

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Welcome to the Game Management</h1>
+    </div>
+</template>
+
+<script setup>
+import { navigateTo } from 'nuxt/app';
+navigateTo('/games'); // Redirect to games page on load
+</script>
